c6129e60174072e4e59e1f48c90e932b95350e6a
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 02:09:47 -0500

    Add table attrs

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -1,5 +1,7 @@
 from pynamodb.models import Model
-from pynamodb.attributes import UnicodeAttribute
+from pynamodb.attributes import (
+    UnicodeAttribute, NumberAttribute,
+)
 
 
 class Csv(Model):
@@ -7,3 +9,8 @@ class Csv(Model):
         table_name = 'csv'
 
     _id = UnicodeAttribute(hash_key=True)
+    serial_number = UnicodeAttribute()
+    company_name = UnicodeAttribute()
+    employee_markme = UnicodeAttribute()
+    description = UnicodeAttribute()
+    leave = NumberAttribute()
