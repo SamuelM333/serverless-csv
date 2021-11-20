92d4707120a4ec097353cf3cf567cd35d34de691
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 02:08:36 -0500

    Add sort key

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,13 +1,19 @@
 module "dynamodb_table" {
   source = "terraform-aws-modules/dynamodb-table/aws"
 
-  name     = "csv"
-  hash_key = "_id"
+  name      = "csv"
+  hash_key  = "_id"
+  range_key = "serial_number"
 
   attributes = [
     {
       name = "_id"
       type = "S"
+    },
+    {
+      name = "serial_number"
+      type = "S"
     }
+
   ]
 }
