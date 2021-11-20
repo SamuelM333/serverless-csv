e9141c27c7f387e0150434c9e1263aa972823a1b
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 02:13:42 -0500

    Fix table attrs

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -1,7 +1,5 @@
 from pynamodb.models import Model
-from pynamodb.attributes import (
-    UnicodeAttribute, NumberAttribute,
-)
+from pynamodb.attributes import UnicodeAttribute
 
 
 class Csv(Model):
@@ -9,8 +7,8 @@ class Csv(Model):
         table_name = 'csv'
 
     _id = UnicodeAttribute(hash_key=True)
-    serial_number = UnicodeAttribute()
+    serial_number = UnicodeAttribute(range_key=True)
     company_name = UnicodeAttribute()
     employee_markme = UnicodeAttribute()
     description = UnicodeAttribute()
-    leave = NumberAttribute()
+    leave = UnicodeAttribute()
