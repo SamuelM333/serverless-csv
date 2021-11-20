0d5cbb3dc08e8f523ae507a6438f7ba20c9f0707
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 02:32:47 -0500

    Add try excepts

--- app.py
+++ app.py
@@ -11,18 +11,31 @@ s3 = boto3.client('s3', region_name='us-east-1')
 app = Chalice(app_name='serverless-csv')
 app.debug = True
 
+# https://www.appsloveworld.com/sample-csv-file/
 @app.on_s3_event(bucket='samuelm333-csv', events=['s3:ObjectCreated:*'])
 def handle_s3_event(event):
     app.log.debug("Received event for bucket: %s, key: %s", event.bucket, event.key)
 
     file = s3.get_object(Bucket=event.bucket, Key=event.key)
 
-    lines = file['Body'].read().decode("utf-8").splitlines(True)
+    try
+        lines = file['Body'].read().decode("utf-8").splitlines(True)
+    except UnicodeDecodeError:
+        app.log.error("Error decoding file")
+        return
+
     reader = list(csv.reader(lines))
 
-    headers = list(map(lambda header: header.lower().replace(" ", "_"), reader.pop(0)))
+    try:
+        headers = list(map(lambda header: header.lower().replace(" ", "_"), reader.pop(0)))
+    except IndexError:
+        app.log.error("IndexError")
+        return
 
     for row in reader:
         data = dict(zip(headers, row))
-        item = Csv(_id=str(uuid4()), **data)
-        item.save()
+        try:
+            item = Csv(_id=str(uuid4()), **data)
+            item.save()
+        except ValueError:
+            app.log.error("ValueError")
