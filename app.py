7c02293daa326fccf0d61371097822775b1ad3cc
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 00:48:59 -0500

    Add s3 event

--- app.py
+++ app.py
@@ -3,27 +3,6 @@ from chalice import Chalice
 app = Chalice(app_name='serverless-csv')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.on_s3_event(bucket='csv', events=['s3:ObjectCreated:*'])
+def handle_s3_event(event):
+    app.log.debug("Received event for bucket: %s, key: %s", event.bucket, event.key)
